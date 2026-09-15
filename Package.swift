e8e7034935fdb476f35ba94763421587bc7ebcce
narci2010 <narci2010@qq.com>
2026-09-15 18:11:40 +0800

    fix: release build errors (pbxproj parse, product name, unsigned IPA)

--- Package.swift
+++ Package.swift
@@ -11,7 +11,7 @@ let package = Package(
     products: [
         .library(name: "AltSign", targets: ["AltSign"]),
         .library(name: "AltServerCore", targets: ["AltServerCore"]),
-        .executable(name: "AltServer", targets: ["AltServerCLI"])
+        .executable(name: "AltServerCLI", targets: ["AltServerCLI"])
     ],
     dependencies: [],
     targets: [

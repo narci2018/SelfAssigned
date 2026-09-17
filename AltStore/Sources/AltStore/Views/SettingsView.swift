b1b3f2ec64e0c02000e1da0b368f5997e5b2bcf2
narci2010 <narci2010@qq.com>
2026-09-17 18:15:57 +0800

    chore: bump version to 1.0.31 to match tag v1.0.31

--- AltStore/Sources/AltStore/Views/SettingsView.swift
+++ AltStore/Sources/AltStore/Views/SettingsView.swift
@@ -245,8 +245,8 @@ struct FeatureRow: View {
 
 // MARK: - Constants
 
-private let altStoreVersion = "1.0.30"
-private let altStoreBuild = "3"
+private let altStoreVersion = "1.0.31"
+private let altStoreBuild = "4"
 
 // MARK: - Preview
 

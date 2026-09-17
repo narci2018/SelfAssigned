b47e437254de44538455da1238e7fc24a7920029
narci2010 <narci2010@qq.com>
2026-09-17 22:40:07 +0800

    chore: bump version to 1.0.55

--- AltStore/Sources/AltStore/Views/SettingsView.swift
+++ AltStore/Sources/AltStore/Views/SettingsView.swift
@@ -245,8 +245,8 @@ struct FeatureRow: View {
 
 // MARK: - Constants
 
-private let altStoreVersion = "1.0.48"
-private let altStoreBuild = "10"
+private let altStoreVersion = "1.0.55"
+private let altStoreBuild = "11"
 
 // MARK: - Preview
 
